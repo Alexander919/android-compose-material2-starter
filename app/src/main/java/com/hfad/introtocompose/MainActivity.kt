196e11e467dda2ae03de1c3ad331393ed202912d
Alexander <a.jegorov84@gmail.com>
2023-10-22 17:42:46 +0100

    tap circle

--- app/src/main/java/com/hfad/introtocompose/MainActivity.kt
+++ app/src/main/java/com/hfad/introtocompose/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.hfad.introtocompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hfad.introtocompose.ui.theme.Material2ThemeStarter
 
 class MainActivity : ComponentActivity() {
@@ -17,14 +41,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Material2ThemeStarter {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    var moneyCounter by remember {
+        mutableIntStateOf(0)
+    }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(color = 0xFF546E7A)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "$${moneyCounter}", style = TextStyle(color = Color.White, fontSize = 35.sp, fontWeight = FontWeight.ExtraBold))
+            Spacer(modifier = Modifier.height(130.dp))
+            CreateCircle {
+                moneyCounter += 5
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun CreateCircle(clicked: () -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .padding(5.dp)
+            .size(100.dp)
+            .clickable(onClick = clicked),
+        shape = CircleShape,
+        elevation = 4.dp
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap")
         }
     }
 }
@@ -41,6 +98,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Material2ThemeStarter {
-        Greeting("Android")
+        MyApp()
     }
 }
